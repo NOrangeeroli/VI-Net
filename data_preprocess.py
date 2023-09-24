81410bf9bd803476cc92e44e1a3b174caed55ff5
NOrangeeroli <1842850321@qq.com>
2023-09-25 00:54:30 +0200

    modified:   data_preprocess.py 	modified:   provider/dataset_pair.py 	modified:   test.py 	modified:   train.py 	modified:   utils/solver.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,13 +1,13 @@
-import json,os
+import json, os, glob
 import _pickle as cPickle
 from collections import defaultdict
 
 
 
 
-def load_stats(data_dir, dataset,list_name):
+def load_stats_train(data_dir, dataset,list_name):
     img_path = os.path.join( dataset,list_name )
-    print(img_path)
+    
     img_list = [os.path.join(data_dir,img_path.split('/')[0], line.rstrip('\n'))
                         for line in open(os.path.join(data_dir, img_path))]
     dict_cat = defaultdict(list)
@@ -19,15 +19,34 @@ def load_stats(data_dir, dataset,list_name):
     
     return dict_cat
 
+def load_stats_test(data_dir, dataset):
+    result_pkl_list = glob.glob(os.path.join(data_dir, 'detection', dataset, 'results_*.pkl'))
+    dict_cat = defaultdict(list) 
+    
+    for path in result_pkl_list:
+        with open(path, 'rb') as f:
+            pred_data = cPickle.load(f)
+        # image_path = os.path.join(data_dir, pred_data['image_path'][5:])
+        for instance,cls  in enumerate(pred_data['pred_class_ids']):
+            dict_cat[int(cls)].append((path, instance))
+    return dict_cat
 
 data_dir = "/media/student/Data/yamei/data/NOCS/"
 
-camera_train_stats = load_stats(data_dir, 'camera', 'train_list.txt')
+camera_train_stats = load_stats_train(data_dir, 'camera', 'train_list.txt')
 with open(os.path.join(data_dir, 'camera', 'train_category_dict.json'), 'w') as fp:
     json.dump(camera_train_stats, fp)
-real_train_stats = load_stats(data_dir, 'real', 'train_list.txt')
+real_train_stats = load_stats_train(data_dir, 'real', 'train_list.txt')
 
 with open(os.path.join(data_dir, 'real', 'train_category_dict.json'), 'w') as fp:
     json.dump(real_train_stats, fp)
 
 
+camera_test_stats = load_stats_test(data_dir, 'CAMERA25')
+import pdb;pdb.set_trace()
+with open(os.path.join(data_dir, 'detection', 'camera_test_category_dict.json'), 'w') as fp:
+    json.dump(camera_test_stats, fp)
+
+real_test_stats = load_stats_test(data_dir, 'REAL275')
+with open(os.path.join(data_dir, 'detection', 'real_test_category_dict.json'), 'w') as fp:
+    json.dump(real_test_stats, fp)
