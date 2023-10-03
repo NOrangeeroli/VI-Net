399093d3f5acc0804525297de6e456ac59b9441e
NOrangeeroli <1842850321@qq.com>
2023-10-03 21:27:13 +0200

    tmp

--- cal_dino.py
+++ cal_dino.py
@@ -21,7 +21,7 @@ sys.path.append(os.path.join(BASE_DIR, 'lib', 'sphericalmap_utils'))
 sys.path.append(os.path.join(BASE_DIR, 'lib', 'pointnet2'))
 
 from solver import test_func, get_logger
-from dataset_pair import TestDataset, TrainingDataset
+from dataset_dino import TestDataset, TrainingDataset
 from evaluation_utils import evaluate
 
 def get_parser():
@@ -86,22 +86,10 @@ if __name__ == "__main__":
     if not os.path.isdir(feature_path):
         os.makedirs(feature_path)
 
-    # model
-    logger.info("=> loading model ...")
     
-    from SIM_Net import Net
-    sim_model = Net(cfg.resolution, cfg.ds_rate)
-    if len(cfg.gpus)>1:
-        
-        sim_model = torch.nn.DataParallel(sim_model, range(len(cfg.gpus.split(","))))
-    
-    sim_model= sim_model.cuda()
 
     
 
-    checkpoint = os.path.join(cfg.log_dir, 'SIM_Net', 'epoch_' + str(cfg.test_epoch) + '.pth')
-    logger.info("=> loading SIM-Net checkpoint from path: {} ...".format(checkpoint))
-    gorilla.solver.load_checkpoint(model=sim_model, filename=checkpoint)
     
     train_dataset  = TrainingDataset(
         cfg.train_dataset,
@@ -109,7 +97,8 @@ if __name__ == "__main__":
         cfg.mod,
         resolution = cfg.resolution,
         ds_rate = cfg.ds_rate,
-        num_img_per_epoch=cfg.num_mini_batch_per_epoch*cfg.train_dataloader.bs, for_sim_feature = True)
+        num_img_per_epoch=cfg.num_mini_batch_per_epoch*cfg.train_dataloader.bs, 
+        )
     # data loader
     train_dataloder = torch.utils.data.DataLoader(
         train_dataset,
@@ -120,25 +109,31 @@ if __name__ == "__main__":
         drop_last=False,
         pin_memory=cfg.train_dataloader.pin_memory
     )
-    feature_arrays = []
-    index_arrays = []
-    cls_arrays = []
+    
+    from extractor_dino import ViTExtractor
+    from torchvision import transforms
+    extractor = ViTExtractor('dinov2_vits14', 14, device = 'cuda')
+    extractor_preprocess = transforms.Normalize(mean=extractor.mean, std=extractor.std)
+    def extract_feature(rgb_raw):
+        rgb_raw = rgb_raw.permute(0,3,1,2)
+        
+        rgb_raw = extractor_preprocess(rgb_raw)
+        
+        
+        with torch.no_grad():
+        
+            dino_feature = extractor.extract_descriptors(rgb_raw, layer = 11, facet = 'token' )
+        
+        dino_feature = dino_feature.reshape(dino_feature.shape[0],6720//14,6720//14,-1)
+        
+        return dino_feature.contiguous()
+    
     with tqdm(total=len(train_dataloder)) as t:
         for i, data in enumerate(train_dataloder):
-            inputs = {
-                        'rgb': data['rgb'].cuda(),
-                        'pts': data['pts'].cuda(),
-                        #'category_label': data['category_label'][0].cuda(),
-                    }
-            with torch.no_grad():
-                features = sim_model.extractor(inputs).cpu()
-            feature_arrays.append(features)
-            index_arrays.append(data['index'])
-            cls_arrays.append(data['cls'])
             
-                        
-
-
+            with torch.no_grad():
+                features = extract_feature(data['rgb']).cpu()
+            import pdb;pdb.set_trace()
 
 
             num_instance = features.shape[0]
@@ -153,20 +148,6 @@ if __name__ == "__main__":
             )
             
             t.update(1)
-        
-    feature_to_write = torch.concatenate(feature_arrays, axis = 0).numpy()
-    index_to_write = torch.concatenate(index_arrays, axis = 0).numpy()
-    cls_to_write = torch.concatenate(cls_arrays, axis = 0).numpy()
-    to_write = np.concatenate([index_to_write, cls_to_write, feature_to_write], axis = 1)
-    
-    save_path = os.path.join(feature_path, cfg.feature.ref_feature_file)
-    invalid_index = np.array(train_dataset.invalid_index)
-    #import pdb;pdb.set_trace()
-    with open(save_path, 'wb') as f:
-        np.save(f, to_write)
-
-    with open(save_path, 'rb') as f:
-        feature = np.load(f)
         
     
     
